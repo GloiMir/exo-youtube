c1a1933a809242ed1f67f0a9e4027d97b87b6ab0
GloiMir <gloirmirindi@gmail.com>
2023-01-13 14:56:41 +0200

    feat:list video gallery

---
+++ js/script.js
@@ -0,0 +1,154 @@
+// KEY_API = "AIzaSyDp1v9Zp7RU5dbk2nLf65GU_yfrjUa6gKs"
+
+let container = document.getElementById('videos_gallery')
+let lecteur = document.getElementById('player')
+let title;
+// lecteur.setAttribute('src','https://www.youtube.com/embed/zz1FiFx3XRE')
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '92afb3d11emsh84a14b583311946p124c6bjsn28b722ba3bf9',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+fetch('https://youtube-v31.p.rapidapi.com/search?q=Fally ipupa&part=snippet%2Cid&regionCode=US&maxResults=50&order=date', options)
+.then(response => response.json())
+.then(response => { 
+	console.log(response.items)  
+	response.items.forEach(element => {
+		// let reader = document.createElement('iframe')
+		// reader.setAttribute('frameBorder',"0")
+		// reader.setAttribute('allowFullScreen',"")
+		// reader.setAttribute('allow',"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture")
+		// reader.setAttribute('loading',"lazy")
+		// reader.setAttribute('src',`https://www.youtube.com/embed/${element.id.videoId}`)
+		// container.appendChild(reader)
+
+		let div1 = document.createElement('a')
+				div1.setAttribute('class',"videos_gallery_item")
+				// div1.setAttribute('onClick',()=> console.log(element.id.videoId))
+				// div1.setAttribute('onClick',"window.location.pathname='/pages/watch.html'")
+				// div1.setAttribute('onClick',"consoe.log('Rien du tout')")
+				// div1.addEventListener('click',()=>lecteur.setAttribute('src','https://www.youtube.com/embed/zz1FiFx3XRE'))
+				div1.setAttribute('href',"./pages/watch.html")
+
+				let div11 = document.createElement('div')
+					div11.setAttribute('class',"video_placeholder")
+					let img1 = document.createElement('img')
+						// img1.setAttribute('src',"./images/vid_placeholder.webp")
+						img1.setAttribute('src',element.snippet.thumbnails.high.url)
+						img1.setAttribute('alt',"")
+					let div111 = document.createElement('div')
+						div111.setAttribute('class',"video_duration") 
+						div111.innerText = "2:13"
+				div11.appendChild(img1)
+				div11.appendChild(div111)
+
+				let div12 = document.createElement('div')
+					div12.setAttribute('class',"videos_details")
+									
+					let div121 = document.createElement('div')
+						div121.setAttribute('class',"chaine_picture")
+						let img2 = document.createElement('img')
+							// img2.setAttribute('src',"./images/avatar.png")
+							img2.setAttribute('src',element.snippet.thumbnails.medium.url)
+							img2.setAttribute('alt',"")
+					div121.appendChild(img2)
+					
+					let div122 = document.createElement('div')
+						let div1221 = document.createElement('div')
+							div1221.setAttribute('class',"videos_title")
+							// div1221.innerText = "C'est pourquoi Ronaldinho a été traité de sorcier"
+							div1221.innerText = element.snippet.title
+						let div1222 = document.createElement('div')
+							div1222.setAttribute('class',"chaine_name")
+							// div1222.innerText = "Joga Bonito"
+							div1222.innerText = element.snippet.channelTitle
+						let div1223 = document.createElement('div')
+							div1223.setAttribute('class',"views_times")
+							div1223.innerText = "4M vues · il y a 4 mois"
+					div122.appendChild(div1221)
+					div122.appendChild(div1222)
+					div122.appendChild(div1223)
+				div12.appendChild(div121)
+				div12.appendChild(div122)
+			div1.appendChild(div11)
+			div1.appendChild(div12)
+
+			container.appendChild(div1)
+	}); 
+})
+.catch(err => console.error(err));
+
+
+// document.getElementById('nom').addEventListener('change',(e)=>title = e.target.value)
+document.getElementById('search_btn').addEventListener('click',()=>{
+	// container
+	fetch(`https://youtube-v31.p.rapidapi.com/search?q=${title}&part=snippet%2Cid&regionCode=US&maxResults=50&order=date`, options)
+	.then(response => response.json())
+	.then(response => {   
+		response.items.forEach(element => {
+			// let reader = document.createElement('iframe')
+			// reader.setAttribute('frameBorder',"0")
+			// reader.setAttribute('allowFullScreen',"")
+			// reader.setAttribute('allow',"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture")
+			// reader.setAttribute('loading',"lazy")
+			// reader.setAttribute('src',`https://www.youtube.com/embed/${element.id.videoId}`)
+			// container.appendChild(reader)
+
+			let div1 = document.createElement('div')
+				div1.setAttribute('class',"videos_gallery_item")
+				div1.setAttribute('onClick',"window.location.pathname='/pages/watch.html'")
+
+				let div11 = document.createElement('div')
+					div11.setAttribute('class',"video_placeholder")
+					let img1 = document.createElement('img')
+						img1.setAttribute('src',"./images/vid_placeholder.webp")
+						img1.setAttribute('alt',"")
+					let div111 = document.createElement('div')
+						div111.setAttribute('class',"video_duration") 
+						div111.innerText = "2:13"
+				div11.appendChild(img1)
+				div11.appendChild(div111)
+
+				// let iframe1 = document.createElement('iframe')
+				// iframe1.setAttribute('id',"player")
+				// iframe1.setAttribute('frameBorder',"0")
+				// iframe1.setAttribute('allowFullScreen',"")
+				// iframe1.setAttribute('allow',"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture")
+				// iframe1.setAttribute('loading',"lazy")
+
+				let div12 = document.createElement('div')
+					div12.setAttribute('class',"videos_details")
+					
+					let div121 = document.createElement('div')
+						div121.setAttribute('class',"chaine_picture")
+						let img2 = document.createElement('img')
+							img2.setAttribute('src',"./images/avatar.png")
+							img2.setAttribute('alt',"")
+					div121.appendChild(img2)
+					
+					let div122 = document.createElement('div')
+						let div1221 = document.createElement('div')
+							div1221.setAttribute('class',"videos_title")
+							div1221.innerText = "C'est pourquoi Ronaldinho a été traité de sorcier"
+						let div1222 = document.createElement('div')
+							div1222.setAttribute('class',"chaine_name")
+							div1222.innerText = "Joga Bonito"
+						let div1223 = document.createElement('div')
+							div1223.setAttribute('class',"views_times")
+							div1223.innerText = "4M vues · il y a 4 mois"
+					div122.appendChild(div1221)
+					div122.appendChild(div1222)
+					div122.appendChild(div1223)
+				div12.appendChild(div121)
+				div12.appendChild(div122)
+			div1.appendChild(div11)
+			div1.appendChild(div12)
+
+			container.appendChild(div1)
+		});  
+	})
+	.catch(err => console.error(err));
+})
